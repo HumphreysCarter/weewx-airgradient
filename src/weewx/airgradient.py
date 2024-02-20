8f295ccd878fba66e25930cd59423fd9fee00c3c
Carter Humphreys <carter.humphreys@lake-effect.dev>
2024-02-19 18:54:45 -0700

    Added unit support.

--- src/weewx/airgradient.py
+++ src/weewx/airgradient.py
@@ -1,5 +1,6 @@
 import json
 import weewx
+import weewx.units
 from weewx.engine import StdService
 from datetime import datetime
 
@@ -12,6 +13,15 @@ class AddAirGradientData(StdService):
 
         # Bind to any new archive record events:
         self.bind(weewx.NEW_ARCHIVE_RECORD, self.new_archive_record)
+
+        # Get unit system in use
+        unit_system = config_dict['StdConvert']['target_unit']  # Options are 'US', 'METRICWX', or 'METRIC'
+        if unit_system == 'US':
+            self.units_temp = weewx.units.USUnits['group_temperature']
+        elif unit_system == 'METRIC':
+            self.units_temp = weewx.units.MetricUnits['group_temperature']
+        elif unit_system == 'METRICWX':
+            self.units_temp = weewx.units.MetricWXUnits['group_temperature']
 
         # Get data from config
         self.api_directory = '/home/pi/air-quality/data/api/'
@@ -61,7 +71,12 @@ class AddAirGradientData(StdService):
                 event.record['ag_out_pm01'] = self.get_value(data, 'pm01')
                 event.record['ag_out_pm02'] = self.get_value(data, 'pm02')
                 event.record['ag_out_pm10'] = self.get_value(data, 'pm10')
-                event.record['ag_out_atmp'] = self.correct_rhum(self.get_value(data, 'atmp'))
+                atmp = self.correct_atmp(self.get_value(data, 'atmp'))
+                if self.units_temp != 'degree_C':
+                    conversion = weewx.units.conversionDict['degree_C'][self.units_temp]
+                    event.record['ag_out_atmp'] = conversion(atmp)
+                else:
+                    event.record['ag_out_atmp'] = atmp
                 event.record['ag_out_rhum'] = self.correct_rhum(self.get_value(data, 'rhum'))
                 event.record['ag_out_wifi'] = self.get_value(data, 'wifi')
                 event.record['ag_out_tvoc'] = self.get_value(data, 'tvoc_index')
@@ -80,6 +95,11 @@ class AddAirGradientData(StdService):
                 event.record['ag_in_pm10'] = self.get_value(data, 'pm10')
                 event.record['ag_in_tvoc'] = self.get_value(data, 'tvoc_index')
                 event.record['ag_in_nox'] = self.get_value(data, 'nox_index')
-                event.record['ag_in_atmp'] = self.get_value(data, 'atmp')
+                atmp = self.get_value(data, 'atmp')
+                if self.units_temp != 'degree_C':
+                    conversion = weewx.units.conversionDict['degree_C'][self.units_temp]
+                    event.record['ag_in_atmp'] = conversion(atmp)
+                else:
+                    event.record['ag_in_atmp'] = atmp
                 event.record['ag_in_rhum'] = self.get_value(data, 'rhum')
                 event.record['ag_in_wifi'] = self.get_value(data, 'wifi')
