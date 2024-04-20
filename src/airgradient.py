0a2c208075761c1539017819ed4c76eb1998dfd7
Carter Humphreys <55723784+HumphreysCarter@users.noreply.github.com>
2024-04-20 03:16:55 -0700

    Updated error handling in airgradient.py

--- src/airgradient.py
+++ src/airgradient.py
@@ -22,15 +22,18 @@ def get_value(data, key, min_value=None, max_value=None):
 
 
 def get_sensor_data(sensor_ip):
-    with urllib.request.urlopen(f'http://{sensor_ip}/measures/current') as url:
-        data = json.load(url)
+    try:
+        with urllib.request.urlopen(f'http://{sensor_ip}/measures/current') as url:
+            data = json.load(url)
 
     # Only return data if data no more than 1 minute old
     # data_date = datetime.strptime(data['datetime'], '%Y-%m-%dT%H:%M:%S.%f')
     # if (datetime.utcnow()-data_date).total_seconds() <= self.stale_minutes * 60:
     #    return data
+        return data
 
-    return data
+    except urllib.error.URLError as e:
+        return None
 
 
 class AddAirGradientData(StdService):
@@ -62,39 +65,39 @@ class AddAirGradientData(StdService):
             data = get_sensor_data(self.outdoor_sensor)
 
             if data is not None:
-                event.record['ag_out_pm01'] = get_value(data, 'pm01')
-                event.record['ag_out_pm02'] = get_value(data, 'pm02')
-                event.record['ag_out_pm10'] = get_value(data, 'pm10')
+                event.record['ag_out_pm01'] = get_value(data, 'pm01', 0)
+                event.record['ag_out_pm02'] = get_value(data, 'pm02', 0)
+                event.record['ag_out_pm10'] = get_value(data, 'pm10', 0)
                 atmp = get_value(data, 'atmp', -100, 100)
-                if self.units_temp != 'degree_C':
+                if atmp is not None and self.units_temp != 'degree_C':
                     conversion = weewx.units.conversionDict['degree_C'][self.units_temp]
                     event.record['ag_out_atmp'] = conversion(atmp)
                 else:
                     event.record['ag_out_atmp'] = atmp
                 event.record['ag_out_rhum'] = get_value(data, 'rhum', 0, 100)
                 event.record['ag_out_wifi'] = get_value(data, 'wifi')
-                event.record['ag_out_tvoc_index'] = get_value(data, 'tvocIndex')
-                event.record['ag_out_tvoc'] = get_value(data, 'tvoc_raw')
-                event.record['ag_out_nox_index'] = get_value(data, 'noxIndex')
-                event.record['ag_out_nox'] = get_value(data, 'nox_raw')
-                event.record['ag_out_rco2'] = get_value(data, 'rco2')
+                event.record['ag_out_tvoc_index'] = get_value(data, 'tvocIndex', 0)
+                event.record['ag_out_tvoc'] = get_value(data, 'tvoc_raw', 0)
+                event.record['ag_out_nox_index'] = get_value(data, 'noxIndex', 0)
+                event.record['ag_out_nox'] = get_value(data, 'nox_raw', 0)
+                event.record['ag_out_rco2'] = get_value(data, 'rco2', 0)
 
         # Get data from indoor sensor
         if self.indoor_sensor is not None:
             data = get_sensor_data(self.indoor_sensor)
 
             if data is not None:
-                event.record['ag_in_rco2'] = get_value(data, 'rco2')
-                event.record['ag_in_pm01'] = get_value(data, 'pm01')
-                event.record['ag_in_pm02'] = get_value(data, 'pm02')
-                event.record['ag_in_pm10'] = get_value(data, 'pm10')
-                event.record['ag_in_tvoc_index'] = get_value(data, 'tvocIndex')
-                event.record['ag_in_tvoc'] = get_value(data, 'tvoc_raw')
-                event.record['ag_in_nox_index'] = get_value(data, 'noxIndex')
-                event.record['ag_in_nox'] = get_value(data, 'nox_raw')
+                event.record['ag_in_rco2'] = get_value(data, 'rco2', 0)
+                event.record['ag_in_pm01'] = get_value(data, 'pm01', 0)
+                event.record['ag_in_pm02'] = get_value(data, 'pm02', 0)
+                event.record['ag_in_pm10'] = get_value(data, 'pm10', 0)
+                event.record['ag_in_tvoc_index'] = get_value(data, 'tvocIndex', 0)
+                event.record['ag_in_tvoc'] = get_value(data, 'tvoc_raw', 0)
+                event.record['ag_in_nox_index'] = get_value(data, 'noxIndex', 0)
+                event.record['ag_in_nox'] = get_value(data, 'nox_raw', 0)
 
                 atmp = get_value(data, 'atmp', -100, 100)
-                if self.units_temp != 'degree_C':
+                if atmp is not None and self.units_temp != 'degree_C':
                     conversion = weewx.units.conversionDict['degree_C'][self.units_temp]
                     event.record['ag_in_atmp'] = conversion(atmp)
                 else:
